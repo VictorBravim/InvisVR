ffc6dafda5f54e901f971572c9b9b6f5b9714a99
Victor Bravim <victorrafaelbravim@gmail.com>
2024-06-03 19:33:41 -0300

    Mudança

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -11,8 +11,9 @@ const Hero: React.FC<HeroProps> = ({ switchSection, currentSection }) => {
     return (
         <div className="h-screen flex flex-col items-center justify-center bg-black-custom relative bg-cover bg-center" style={{ backgroundImage: "url('/bg.png')", }}>
             <div className='flex flex-col justify-center items-center'>
-                <div className="w-full h-[50%] absolute top-16">
+                <div className="w-full h-[80%] absolute top-0">
                     <iframe src='https://my.spline.design/untitled-a3b57b9566980b78009fda005e3986ff/' frameBorder='0' width='100%' height='100%'></iframe>
+                    <div className="absolute bottom-0 right-0 w-48 h-16 bg-black-custom"></div>
                 </div>
                 <div className='absolute flex flex-col justify-center items-center bottom-12'>
                     <h1 className="text-8xl text-center font-bold text-white mb-4">Embark on <span className='text-purple-700'>Virtual <br /> Journeys</span></h1>
