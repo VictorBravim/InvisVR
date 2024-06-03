e994b056f0e6399bf47edfe57ccfea9489af4781
Victor Bravim <victorrafaelbravim@gmail.com>
2024-06-03 14:57:14 -0300

    Mudança

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -9,7 +9,7 @@ interface AboutProps {
 
 const About: React.FC<AboutProps> = ({ switchSection, currentSection }) => {
     return (
-        <div className="h-screen flex flex-col items-center justify-center bg-black relative">
+        <div className="h-screen flex flex-col items-center justify-center bg-black-custom relative">
             <h1 className="text-4xl font-bold text-white mb-8">About Section</h1>
             <Nav switchSection={switchSection} currentSection={currentSection} />
         </div>
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -9,8 +9,21 @@ interface HeroProps {
 
 const Hero: React.FC<HeroProps> = ({ switchSection, currentSection }) => {
     return (
-        <div className="h-screen flex flex-col items-center justify-center bg-black relative">
-            <h1 className="text-4xl font-bold text-white mb-8">Hero Section</h1>
+        <div className="h-screen flex flex-col items-center justify-center bg-black-custom relative">
+            {/* Título grande */}
+            <h1 className="text-6xl font-bold text-white mb-4">Embark on Virtual Journeys</h1>
+
+            {/* Botão */}
+            <button className="bg-blue-500 text-white px-4 py-2 rounded-lg mb-4">Explore Now</button>
+
+            {/* Texto curto */}
+            <p className="text-white text-lg text-center mb-8">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+
+            {/* iframe para o 3D do spline */}
+            <div className="w-full h-full absolute top-0 left-0">
+                <iframe src='https://my.spline.design/untitled-a3b57b9566980b78009fda005e3986ff/' frameBorder='0' width='50%' height='50%'></iframe>
+            </div>
+
             <Nav switchSection={switchSection} currentSection={currentSection} />
         </div>
     );
