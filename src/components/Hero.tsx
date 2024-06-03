d4ef64781e99d0197ba141dfa6157ec51f4c2459
Victor Bravim <victorrafaelbravim@gmail.com>
2024-06-03 16:50:41 -0300

    Mudança

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -9,21 +9,17 @@ interface HeroProps {
 
 const Hero: React.FC<HeroProps> = ({ switchSection, currentSection }) => {
     return (
-        <div className="h-screen flex flex-col items-center justify-center bg-black-custom relative">
-            {/* Título grande */}
-            <h1 className="text-6xl font-bold text-white mb-4">Embark on Virtual Journeys</h1>
-
-            {/* Botão */}
-            <button className="bg-blue-500 text-white px-4 py-2 rounded-lg mb-4">Explore Now</button>
-
-            {/* Texto curto */}
-            <p className="text-white text-lg text-center mb-8">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-
-            {/* iframe para o 3D do spline */}
-            <div className="w-full h-full absolute top-0 left-0">
-                <iframe src='https://my.spline.design/untitled-a3b57b9566980b78009fda005e3986ff/' frameBorder='0' width='50%' height='50%'></iframe>
+        <div className="h-screen flex flex-col items-center justify-center bg-black-custom relative bg-cover bg-center" style={{ backgroundImage: "url('/bg.png')", }}>
+            <div className='flex flex-col justify-center items-center'>
+                <div className="w-full h-[50%] absolute top-16">
+                    <iframe src='https://my.spline.design/untitled-a3b57b9566980b78009fda005e3986ff/' frameBorder='0' width='100%' height='100%'></iframe>
+                </div>
+                <div className='absolute flex flex-col justify-center items-center bottom-12'>
+                    <h1 className="text-8xl text-center font-bold text-white mb-4">Embark on <span className='text-purple-700'>Virtual <br /> Journeys</span></h1>
+                    <p className="text-white text-lg text-center mb-8">Lorem ipsum dolor sit amet, consectetur adipiscing elit <br /> Lorem ipsum dolor sit amet.</p>
+                    <button className="bg-purple-700 text-white px-4 py-2 rounded-lg mb-4">Explore Now</button>
+                </div>
             </div>
-
             <Nav switchSection={switchSection} currentSection={currentSection} />
         </div>
     );
